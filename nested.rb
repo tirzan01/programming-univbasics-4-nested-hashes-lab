787685452644bceaa5ca539b9afb76a7e6ae5fd3
thomas <thomasmonta98@gmail.com>
2021-08-23 14:07:43 +1000

    done with assignemnt

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
       }
     }
 
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
       }
     }
 
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,6 +58,7 @@ def dennis_ritchies_language
       }
     }
 
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -82,6 +85,12 @@ def adding_matz
       }
     }
 
+    programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ['LISP', 'C']
+  }
+
+  programmer_hash
 end
 
 def changing_alan
@@ -104,6 +113,10 @@ def changing_alan
       }
     }
 
+  programmer_hash[:alan_kay][:known_for] = 'GUI'
+
+  programmer_hash
+
 end
 
 def adding_to_dennis
@@ -125,5 +138,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+  programmer_hash[:dennis_ritchie][:languages] << 'Assembly'
+
+  programmer_hash
 
 end
